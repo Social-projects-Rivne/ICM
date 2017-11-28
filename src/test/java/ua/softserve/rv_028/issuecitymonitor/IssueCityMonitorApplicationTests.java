0715b25b0688fbf49703210b6f7c2abaabb52cad
Olexandr Kovalchuk <vinni89@ukr.net>
2017-11-28 15:13:45 +0200

    add sceleton

---
+++ src/test/java/ua/softserve/rv_028/issuecitymonitor/IssueCityMonitorApplicationTests.java
@@ -0,0 +1,16 @@
+package ua.softserve.rv_028.issuecitymonitor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IssueCityMonitorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
