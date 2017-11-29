f56b818bfe8d60c0b2ea6907773eb618a942588a
Evhenii Taranukha <tzhenia@www.rnpp.rv.ua>
2017-11-29 16:53:26 +0200

    integrated  react app with Spring Boot

---
+++ src/main/resources/static/src/components/Sidebar/_nav.js
@@ -0,0 +1,60 @@
+export default {
+  items: [
+
+    {
+      name: 'Dashboard',
+      url: '/dashboard',
+      icon: 'icon-home',
+
+    },
+
+
+    {
+      name: 'Issues',
+      url: '/issues',
+      icon: 'icon-layers',
+      badge: {
+        variant: 'info',
+        text: 'NEW'
+      }
+    },
+
+    {
+      name: 'Petition',
+      url: '/petition',
+      icon: 'icon-list',
+      badge: {
+        variant: 'info',
+        text: 'NEW'
+      }
+    },
+
+    {
+      name: 'Events',
+      url: '/events',
+      icon: 'icon-event',
+      badge: {
+        variant: 'info',
+        text: 'NEW'
+      }
+    },
+
+    {
+      name: 'Users',
+      url: '/users',
+      icon: 'icon-people',
+      badge: {
+        variant: 'info',
+        text: 'NEW'
+      }
+    },
+
+    {
+      name: 'Settings',
+      url: '/settings',
+      icon: 'icon-wrench',
+    },
+
+
+  ]
+};
