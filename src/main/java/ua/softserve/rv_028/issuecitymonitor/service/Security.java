60f195a406c55d1640f724854db85c8932139894
gefasim <gefasim@mail.com>
2017-12-03 17:36:58 +0200

    Provide login in system from database

--- src/main/java/ua/softserve/rv_028/issuecitymonitor/service/Security.java
+++ src/main/java/ua/softserve/rv_028/issuecitymonitor/service/Security.java
@@ -1,6 +1,8 @@
 package ua.softserve.rv_028.issuecitymonitor.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,11 +11,30 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 @EnableWebSecurity
 public class Security extends WebSecurityConfigurerAdapter{
+
+    private final UserDetailService userDetailsService;
+
+    @Autowired
+    public Security(UserDetailService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.
+                userDetailsService(userDetailsService);
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/").authenticated()
+                .antMatchers("/dashboard").authenticated()
                 .antMatchers("/issues").authenticated()
+                .antMatchers("/petitions").authenticated()
+                .antMatchers("/events").authenticated()
+                .antMatchers("/users").authenticated()
+                .antMatchers("/settings").authenticated()
                 .anyRequest().permitAll()
 
                 .and().csrf().disable().formLogin()
