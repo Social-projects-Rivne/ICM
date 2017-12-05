c228a9f19a5d6d7951371d6bf6f4f06b7ff7b848
Olexandr Kovalchuk <vinni89@ukr.net>
2017-12-05 21:43:16 +0200

    fix problems after pull from dev

--- src/main/resources/static/src/views/Issues/Issues.js
+++ src/main/resources/static/src/views/Issues/Issues.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
-class Issues extends Component {
 
-  render() {
-    return (
-      <div className="animated fadeIn">
-        Hello Issues
-      </div>
+class Issue extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            issueList: this.load(),
+            issues : "Some init"
+        };
+        this.load = this.load.bind(this);
+    }
+
+    load(event){
+        axios.get("api/issues")
+            .then(
+                (response) => {
+            this.setState({issues: response.data});
+        console.log(response);
+    });
+    }
+
+    render() {
+        return (
+            <div className="animated fadeIn">
+            {this.state.issues}
+    </div>
     )
-  }
+    }
 }
 
-export default Issues;
+export default Issue;
