346bc0f1bcf4fd15d6b8242a6064f682548a1c16
Olexandr Kovalchuk <vinni89@ukr.net>
2017-12-06 02:28:55 +0200

    new Table

--- src/main/resources/static/src/views/Issues/Issues.js
+++ src/main/resources/static/src/views/Issues/Issues.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import {Button, Card, CardBody, CardHeader, Col, Row, Table} from "reactstrap";
 
-
-class Issue extends Component {
+class Issues extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
             issueList: this.load(),
-            issues : "Some init"
+            issues : []
         };
         this.load = this.load.bind(this);
     }
@@ -25,10 +25,34 @@ class Issue extends Component {
     render() {
         return (
             <div className="animated fadeIn">
-            {this.state.issues}
+                <Row>
+                    <Col xs="12" lg="12">
+                        <Card>
+                            <CardHeader>Issues list</CardHeader>
+                            <CardBody>
+                                <Table responsive bordered>
+                                    <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Title</th>
+                                        <th>Initial Date</th>
+                                        <th>End Date</th>
+                                        <th>Category</th>
+                                        <th>Actions</th>
+                                        <th>User ID</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    {this.state.issues}
+                                    </tbody>
+                                </Table>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
     </div>
     )
     }
 }
 
-export default Issue;
+export default Issues;
