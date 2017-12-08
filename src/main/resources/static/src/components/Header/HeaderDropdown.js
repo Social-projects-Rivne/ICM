537b946a931bf583340ffce224cf741dcf56c4ae
gefasim <gefasim@mail.com>
2017-12-08 21:23:46 +0200

    Remove unused models and components

--- src/main/resources/static/src/components/Header/HeaderDropdown.js
+++ src/main/resources/static/src/components/Header/HeaderDropdown.js
@@ -45,19 +45,8 @@ class HeaderDropdown extends Component {
         <img src={'/build/img/avatars/1.jpg'} id='admin-logo' className="img-avatar" />
             </DropdownToggle>
             <DropdownMenu right>
-        <DropdownItem header tag="div" className="text-center"><strong>Account</strong></DropdownItem>
-        <DropdownItem><i className="fa fa-bell-o"/> Updates<Badge color="info">42</Badge></DropdownItem>
-        <DropdownItem><i className="fa fa-envelope-o"/> Messages<Badge color="success">42</Badge></DropdownItem>
-        <DropdownItem><i className="fa fa-tasks"/> Tasks<Badge color="danger">42</Badge></DropdownItem>
-        <DropdownItem><i className="fa fa-comments"/> Comments<Badge color="warning">42</Badge></DropdownItem>
-        <DropdownItem header tag="div" className="text-center"><strong>Settings</strong></DropdownItem>
-        <DropdownItem><i className="fa fa-user"/> Profile</DropdownItem>
-            <DropdownItem><i className="fa fa-wrench"/> Settings</DropdownItem>
-            <DropdownItem><i className="fa fa-usd"/> Payments<Badge color="secondary">42</Badge></DropdownItem>
-        <DropdownItem><i className="fa fa-file"/> Projects<Badge color="primary">42</Badge></DropdownItem>
-        <DropdownItem divider/>
-        <DropdownItem><i className="fa fa-shield"/> Lock Account</DropdownItem>
-        <DropdownItem onClick={this.handleLogoutClick}><i className="fa fa-lock"/> Logout </DropdownItem>
+                <DropdownItem header tag="div" className="text-center"><strong>Account</strong></DropdownItem>
+                <DropdownItem onClick={this.handleLogoutClick}><i className="fa fa-lock"/> Logout </DropdownItem>
             </DropdownMenu>
             </Dropdown>
     );
--- src/main/resources/static/src/containers/Full/Full.js
+++ src/main/resources/static/src/containers/Full/Full.js
@@ -4,7 +4,6 @@ import {Container} from 'reactstrap';
 import Header from '../../components/Header/';
 import Sidebar from '../../components/Sidebar/';
 import Breadcrumb from '../../components/Breadcrumb/';
-import Aside from '../../components/Aside/';
 import Footer from '../../components/Footer/';
 
 import Dashboard from '../../views/Dashboard/';
@@ -32,7 +31,6 @@ class Full extends Component {
               </Switch>
             </Container>
           </main>
-          {/*<Aside />*/}
         </div>
         <Footer />
       </div>
