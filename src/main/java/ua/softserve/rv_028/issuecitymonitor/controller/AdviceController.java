d481644f53c89b2dbbce5d34bb97706be370303d
gefasim <gefasim@mail.com>
2017-12-17 23:33:25 +0200

    Restore registrationHandler reason for AdviceController

--- src/main/java/ua/softserve/rv_028/issuecitymonitor/controller/AdviceController.java
+++ src/main/java/ua/softserve/rv_028/issuecitymonitor/controller/AdviceController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import ua.softserve.rv_028.issuecitymonitor.Constants;
 import ua.softserve.rv_028.issuecitymonitor.exception.RegistrationException;
 
 @ControllerAdvice
@@ -19,7 +20,7 @@ public class AdviceController {
     }
 
 
-    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = Constants.REGISTRATION_FAIL_REASON)
     @ExceptionHandler(RegistrationException.class)
     public void registrationError(RegistrationException e){
         LOGGER.error(e.getMessage());
