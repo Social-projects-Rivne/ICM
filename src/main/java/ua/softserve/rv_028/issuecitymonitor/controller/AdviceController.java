08deb2785fa59ad74229f1519e12638583d85ee0
gefasim <gefasim@mail.com>
2017-12-18 12:36:18 +0200

    Create RestorePasswordException class

--- src/main/java/ua/softserve/rv_028/issuecitymonitor/controller/AdviceController.java
+++ src/main/java/ua/softserve/rv_028/issuecitymonitor/controller/AdviceController.java
@@ -2,11 +2,16 @@ package ua.softserve.rv_028.issuecitymonitor.controller;
 
 import org.apache.log4j.Logger;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import ua.softserve.rv_028.issuecitymonitor.Constants;
 import ua.softserve.rv_028.issuecitymonitor.exception.RegistrationException;
+import ua.softserve.rv_028.issuecitymonitor.exception.RestorePasswordException;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @ControllerAdvice
 public class AdviceController {
@@ -24,6 +29,13 @@ public class AdviceController {
     @ExceptionHandler(RegistrationException.class)
     public void registrationError(RegistrationException e){
         LOGGER.error(e.getMessage());
+    }
+
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(RestorePasswordException.class)
+    public void registrationError(RestorePasswordException e, HttpServletResponse response) throws IOException {
+        LOGGER.error(e.getMessage());
+        response.sendError(HttpStatus.BAD_REQUEST.value(), e.getMessage());
     }
 }
 
