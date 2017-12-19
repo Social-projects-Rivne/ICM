124386a5e97f7062f48ec585a1d56f4ecd173d8c
gefasim <gefasim@mail.com>
2017-12-19 16:38:18 +0200

    Add Footer and Container switch

--- src/main/resources/static/src/containers/Client/Client.js
+++ src/main/resources/static/src/containers/Client/Client.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import {Link, Switch, Route, Redirect} from 'react-router-dom';
+import {Container} from 'reactstrap';
 import ClientHeader from '../../components/ClientHeader/ClientHeader'
+import Footer from '../../components/Footer/';
 
 export default class SignUp extends Component{
     render(){
         return(
             <div className="app">
                 <ClientHeader />
+                <div className="app-body">
+                    <Container fluid>
+                        <Switch>
+
+                        </Switch>
+                    </Container>
+                </div>
+                <Footer />
             </div>
         )
     }
