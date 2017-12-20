3f428f2d1163cf19a147733b942647a257500ddb
gefasim <gefasim@mail.com>
2017-12-20 13:48:15 +0200

    Add anonymous Header for Authorization containers

--- src/main/resources/static/src/components/ClientHeader/ClientHeader.js
+++ src/main/resources/static/src/components/ClientHeader/ClientHeader.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {Nav, NavbarBrand, NavItem, NavLink, NavbarToggler,} from 'reactstrap';
 import ClientHeaderDropdown from './ClientHeaderDropdown';
 
-export default class Header extends Component {
+export default class ClientHeader extends Component {
 
     render() {
         return(
@@ -14,9 +14,17 @@ export default class Header extends Component {
                 <NavLink href="/">Petitions</NavLink>
 
                 <Nav className="ml-auto" navbar>
-                    <ClientHeaderDropdown userAuthorities={this.props.userAuthorities}/>
+                    {ClientHeader.renderHeaderDropdown(this.props.userAuthorities)}
                 </Nav>
             </header>
         )
+    }
+
+    static renderHeaderDropdown(authorities){
+        if(authorities != null){
+            if (!authorities.some(function(auth){return auth.authority === "ROLE_ANONYMOUS"})){
+                return <ClientHeaderDropdown userAuthorities={authorities}/>
+            }
+        }
     }
 }
