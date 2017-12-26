b98a4c26c5173ce3e9b42ff5df9fb9655bc5666b
Olexandr Kovalchuk <vinni89@ukr.net>
2017-12-27 01:15:53 +0200

    add new React Components

---
+++ src/main/resources/static/src/components/PageContainer/PageContainer.js
@@ -0,0 +1,101 @@
+import React, {Component} from 'react';
+import {Card, CardBody, CardFooter, CardHeader, Col, Pagination, PaginationItem, PaginationLink, Row} from "reactstrap";
+
+class PageContainer extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            pagesNum: this.props.pagesNum,
+            page: this.props.page,
+            title: this.props.title,
+            children: this.props.children
+        };
+
+        this.handleFirstPage = this.handleFirstPage.bind(this);
+        this.handleLastPage = this.handleLastPage.bind(this);
+        this.handlePage = this.handlePage.bind(this);
+    }
+
+    componentWillReceiveProps(props){
+        this.setState({
+            pagesNum: props.pagesNum,
+            page: props.page,
+            title: props.title,
+            children: props.children
+        });
+    }
+
+    handlePage(i) {
+        this.changePage(i);
+    }
+
+    handleFirstPage() {
+        this.changePage(0);
+    }
+
+    handleLastPage() {
+        this.changePage(this.state.pagesNum);
+    }
+
+    changePage(pageNum) {
+        if(this.state.page !== pageNum) {
+            this.setState({page: pageNum}, function () {
+                this.props.onPageChange(this.state.page);
+            });
+        }
+    }
+
+    pagination() {
+        var items = [];
+        for(var i = (this.state.page - 2 > 0) ? (this.state.page - 2) : 1;
+            i < ((this.state.pagesNum - this.state.page > 2) ? (this.state.page + 2) : this.state.pagesNum) + 1;
+            i++) {
+            items.push(i);
+        }
+
+        var _this = this;
+
+        return (
+            <Pagination size="sm">
+                <PaginationItem disabled={this.state.page === 1}>
+                    <PaginationLink previous onClick={this.handleFirstPage} />
+                </PaginationItem>
+                {items.map(function (item, i) {
+                    return (
+                        <PaginationItem key={i} active={item === _this.state.page}>
+                            <PaginationLink key={i} onClick={() => _this.handlePage(item)}>
+                                {item}
+                            </PaginationLink>
+                        </PaginationItem>
+                    )
+                })}
+                <PaginationItem disabled={this.state.pagesNum === this.state.page || this.state.pagesNum === 0}>
+                    <PaginationLink next onClick={this.handleLastPage} />
+                </PaginationItem>
+            </Pagination>
+        )
+    }
+
+    render() {
+        return (
+            <div className="animated fadeIn">
+                <Row>
+                    <Col xs="12" lg="12">
+                        <Card>
+                            <CardHeader>{this.state.title}</CardHeader>
+                            <CardBody>
+                                {this.state.children}
+                            </CardBody>
+                            <CardFooter>
+                                {this.pagination()}
+                            </CardFooter>
+                        </Card>
+                    </Col>
+                </Row>
+            </div>
+        )
+    }
+}
+
+export default PageContainer;
--- src/main/resources/static/src/views/Issues/Issues.js
+++ src/main/resources/static/src/views/Issues/Issues.js
@@ -1,32 +1,34 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import axios from 'axios';
-import {Button, Card, CardBody, CardHeader, CardFooter, Col, Row, Table} from "reactstrap";
 import swal from 'sweetalert';
-import {Link} from "react-router-dom";
+import IssuesContainer from "./IssuesContainer";
 
 class Issues extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            issues: [],
+            issues: "",
             page: 0,
-            size: 10,
-            step: 1
-        }
-        this.handleNavFirst = this.handleNavFirst.bind(this);
-        this.handleNavPrev = this.handleNavPrev.bind(this);
-        this.handleNavNext = this.handleNavNext.bind(this);
-        this.handleNavLast = this.handleNavLast.bind(this);
+            size: 10
+        };
+
+        this.handlePageChange = this.handlePageChange.bind(this);
     }
 
     componentWillMount() {
-        this.onNavigate();
+        this.makeQuery();
     }
 
-    onNavigate() {
+    handlePageChange(pageNum) {
+        this.setState({page: pageNum}, function() {
+            this.makeQuery();
+        });
+    }
+
+    makeQuery() {
         var _this = this;
-        axios.get(["/api/issues?page=", this.state.page, "&size=", this.state.size].join(""))
+        axios.get(["/api/issues?page=", this.state.page, "$size=", this.state.size].join(""))
             .then(function(response) {
                 _this.setState({
                     issues: response.data.content
@@ -36,125 +38,10 @@ class Issues extends Component {
                 swal({title: "Something went wrong!", text: error, icon: "error"});
             });
     }
-
-    handleNavFirst(e) {
-        e.preventDefault();
-        this.setState({page: 0});
-        this.onNavigate();
-    }
-
-    handleNavPrev(e) {
-        e.preventDefault();
-    	this.setState({page: this.state.page - this.state.step});
-    	this.onNavigate();
-    }
-
-    handleNavNext(e) {
-        e.preventDefault();
-    	this.setState({page: this.state.page + this.state.step});
-    	this.onNavigate();
-    }
-
-    handleNavLast(e) {
-        e.preventDefault();
-        this.setState({page: this.state.page});
-        this.onNavigate();
-    }
-
     render() {
-
         return (
-            <div className="animated fadeIn">
-                <Row>
-                    <Col xs="12" lg="12">
-                        <Card>
-                            <CardHeader>Issue list</CardHeader>
-                            <CardBody>
-                                <Table responsive bordered>
-                                    <thead>
-                                    <tr>
-                                        <th>#</th>
-                                        <th>Title</th>
-                                        <th>Initial Date</th>
-                                        <th>Category</th>
-                                        <th>User ID</th>
-                                        <th>Actions</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                        {this.state.issues.map(function(issue, i) {
-                                            return (<Issue key={i} issue={issue}/>);
-                                        })}
-                                    </tbody>
-                                </Table>
-                            </CardBody>
-                            <CardFooter className="text-center">
-                                <Button color="info" onClick={this.handleNavFirst}>First</Button>
-                                <Button color="info" onClick={this.handleNavPrev}>Previous</Button>
-                                <Button color="info" onClick={this.handleNavNext}>Next</Button>
-                                <Button color="info" onClick={this.handleNavLast}>Last</Button>
-                            </CardFooter>
-                        </Card>
-                    </Col>
-                </Row>
-            </div>
+            <IssuesContainer data={this.state.issues} onPageChange={this.handlePageChange}/>
         )
     }
 }
-
-class Issue extends Component {
-
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            issue: this.props.issue,
-            mounted: true
-        };
-
-        this.handleDelete = this.handleDelete.bind(this);
-    }
-
-    handleDelete(){
-        var _this = this;
-        swal({
-            title: "Are you sure?",
-            text: "Once deleted, you will not be able to recover this data!",
-            icon: "warning",
-            buttons: true,
-            dangerMode: true,
-        })
-            .then(function(willDelete) {
-                if (willDelete) {
-                    axios.delete("/api/issues/"+_this.state.issue.id)
-                        .then(function(response) {
-                            _this.setState({
-                                mounted: false
-                            });
-                            swal({title: "Issue record deleted", icon: "success"});
-                        }).catch(function (error) {
-                        swal({title: "Something went wrong!", text: error, icon: "error"});
-                    });
-                }
-            });
-    }
-
-    render() {
-        const row = (
-            <tr>
-                <td>{this.state.issue.id}</td>
-                <td>{this.state.issue.title}</td>
-                <td>{this.state.issue.initialDate}</td>
-                <td>{this.state.issue.category}</td>
-                <td>{this.state.issue.userDto.id}</td>
-                <td>
-                    <Link to={"/admin/issues/"+this.state.issue.id+"/edit"}><Button color="info" size="sm">Edit</Button></Link>{' '}
-                    <Button color="danger" size="sm" onClick={this.handleDelete}>Delete</Button>
-                </td>
-            </tr>
-        );
-        return (this.state.mounted === true ? row : null)
-    }
-}
-
 export default Issues;
