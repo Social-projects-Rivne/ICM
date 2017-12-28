31cd0baca427d34541f83d3622283b6db4ed6267
MSennikov <koliasen73@gmail.com>
2017-12-28 13:30:45 +0200

    Add markers

--- src/main/resources/static/src/views/IssueMap/IssueMap.js
+++ src/main/resources/static/src/views/IssueMap/IssueMap.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
 import { compose, withProps } from "recompose"
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
-
+import axios from 'axios';
+import swal from 'sweetalert';
 
 class IssueMap extends Component {
+    constructor(props){
+        super(props);
+
+
+        this.state = {
+            issues: []
+        };
+    }
+
+    componentWillMount() {
+        var _this = this;
+        axios.get("/api/issues")
+            .then(function(response) {
+                _this.setState({
+                    issues: response.data
+                });
+            })
+            .catch(function (error) {
+                swal({title: "Something went wrong!", text: error, icon: "error"});
+            });
+    }
+
    render() {
     const MyMapComponent = compose(
       withProps({
@@ -19,7 +42,14 @@ class IssueMap extends Component {
         defaultZoom={8}
         defaultCenter={{ lat: -34.397, lng: 150.644 }}
       >
+          {this.state.issues.map(issues => (
+              <Marker
+                  key={issues.id}
+                  position={{ lat: issues.latitude, lng: issues.longitude }}
+              />
+          ))}
       </GoogleMap>
+
     )
         return (
             <MyMapComponent/>
