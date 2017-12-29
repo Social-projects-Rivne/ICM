c345abe0405d09a4b657b04f27871e6a46076aef
=MSennikov <=koliasen73@gmail.com>
2017-12-29 16:28:02 +0200

    Add responsive map

--- src/main/resources/static/src/views/IssueMap/IssueMap.js
+++ src/main/resources/static/src/views/IssueMap/IssueMap.js
@@ -5,38 +5,11 @@ import swal from 'sweetalert';
 import Map from './Map';
 
 class IssueMap extends Component {
-    constructor(props){
-        super(props);
-
-
-        this.state = {
-            issues: [],
-            width: props.width,
-            height: props.height
-        };
-
-    }
-
-    componentWillMount() {
-        this.setState({height: window.innerHeight + 'px', width: window.innerWidth + 'px'});
-
-        var _this = this;
-        axios.get("/api/issues")
-            .then(function(response) {
-                _this.setState({
-                    issues: response.data,
-                    isOpen: false
-                });
-            })
-            .catch(function (error) {
-                swal({title: "Something went wrong!", text: error, icon: "error"});
-            });
-    }
-
    render() {
 
    return(
-        <div style={{height: this.state.height, weight: this.state.width}}>
+        <div className="map-responsive">
+
             <Map
                 googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAMDIyzcpoHdvK8MLCUhvrqZDyllGiyrnM &callback=initMap"
                 loadingElement={<div style={{ height: `100%` }} />}
