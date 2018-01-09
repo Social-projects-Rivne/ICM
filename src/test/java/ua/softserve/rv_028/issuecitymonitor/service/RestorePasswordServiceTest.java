5b2af421bb0b766793535c20bb7de70e9d6a9a5e
gefasim <gefasim@mail.com>
2018-01-09 12:18:56 +0200

    Update RestorePasswordServiceTest

--- src/test/java/ua/softserve/rv_028/issuecitymonitor/service/RestorePasswordServiceTest.java
+++ src/test/java/ua/softserve/rv_028/issuecitymonitor/service/RestorePasswordServiceTest.java
@@ -1,82 +1,76 @@
 package ua.softserve.rv_028.issuecitymonitor.service;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import ua.softserve.rv_028.issuecitymonitor.TestApplication;
+import ua.softserve.rv_028.issuecitymonitor.TestUtils;
 import ua.softserve.rv_028.issuecitymonitor.dao.RestorePasswordDao;
 import ua.softserve.rv_028.issuecitymonitor.dao.UserDao;
-import ua.softserve.rv_028.issuecitymonitor.dto.UserDto;
 import ua.softserve.rv_028.issuecitymonitor.entity.RestorePassword;
 import ua.softserve.rv_028.issuecitymonitor.entity.User;
 import ua.softserve.rv_028.issuecitymonitor.exception.RestorePasswordException;
-import ua.softserve.rv_028.issuecitymonitor.service.mappers.UserMapper;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = TestApplication.class)
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = {TestApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class RestorePasswordServiceTest {
 
-    private User user;
-    private User user2;
-    private UserDto user2Dto;
-    private static final String NO_TOKEN = "NO_TOKEN";
-    private static final String TOKEN = "EXAMPLE_TOKEN";
+    private static User USER;
 
     @Autowired
     private UserDao userDao;
 
     @Autowired
-    private RestorePasswordDao restorePasswordDao;
+    private RestorePasswordService service;
 
     @Autowired
-    private UserMapper mapper;
+    private RestorePasswordDao restorePassDao;
 
     @Autowired
-    private RestorePasswordService restorePassword;
+    private BCryptPasswordEncoder encoder;
 
     @Before
     public void setup(){
-        user = userDao.findAll().get(0);
-        user2 = userDao.findAll().get(1);
-        user2Dto = mapper.toDto(user2);
-        restorePasswordDao.deleteByUser(user2);
-        //restorePasswordDao.save(new RestorePassword(user2, TOKEN));
-    }
-
-    //TODO
-    /*@Test
-    public void createOrderRestorePasswordTest(){
-        boolean response = restorePassword.createResetToken(user.getUsername());
-        assertEquals(true, response);
-    }*/
-
-    @Test(expected = RestorePasswordException.class)
-    public void createOrderRestorePasswordFailTest(){
-        String NOT_EXIST_EMAIL = "no-email!";
-        restorePassword.createResetToken(NOT_EXIST_EMAIL);
-    }
-
-    @Test(expected = RestorePasswordException.class)
-    public void setNewPasswordTestEmptyUser(){
-        UserDto emptyUser = new UserDto();
-        restorePassword.setNewPasswordForUser(emptyUser.getEmail(), emptyUser.getPassword(), NO_TOKEN);
-    }
-
-    @Test(expected = RestorePasswordException.class)
-    public void setNewPasswordTestEmptyPassword(){
-        UserDto userEmptyPassword = mapper.toDto(user);
-        userEmptyPassword.setPassword("");
-        restorePassword.setNewPasswordForUser(userEmptyPassword.getEmail(), userEmptyPassword.getPassword(),
-                NO_TOKEN);
+        USER = userDao.save(TestUtils.createUser(0));
     }
 
     @Test
-    public void setNewPasswordTest(){
-        String newPass = "new_pass";
-        RestorePassword restorePassDto = restorePasswordDao.findByUser(user2);
-        restorePassword.setNewPasswordForUser(user2Dto.getEmail(), newPass, restorePassDto.getToken());
+    public void setNewPasswordForUser(){
+        restorePassDao.deleteByUser(USER);
+        service.createResetToken(USER.getUsername());
+        String token = restorePassDao.findByUser(USER).getToken();
+        service.setNewPasswordForUser(USER.getUsername(), "newPassword", token);
+        User updatedUser = userDao.findUserByUsername(USER.getUsername());
+        assertEquals(true, encoder.matches("newPassword", updatedUser.getPassword()));
+    }
+
+    @Test(expected = RestorePasswordException.class)
+    public void setNewPasswordForUserFail(){
+        service.setNewPasswordForUser(null, null, null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void setNewPasswordForUserFailToken(){
+        service.setNewPasswordForUser(USER.getUsername(), "newPassword", "incorrectPassword");
+    }
+
+    @Test
+    public void createResetToken(){
+        service.createResetToken(USER.getUsername());
+        RestorePassword restorePassword = restorePassDao.findByUser(USER);
+        assertEquals(false, restorePassword.getToken().isEmpty());
+    }
+
+    @After
+    public void cleanup(){
+        restorePassDao.deleteAll();
+        userDao.deleteAll();
     }
 }
