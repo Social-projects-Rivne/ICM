db54937138a430a631d6e4a5443427fdb63aeda4
Evhenii Taranukha <tzhenia@MacBook-Air-tzhenia.local>
2018-01-15 17:24:40 +0200

    edited PDF controller

--- src/main/java/ua/softserve/rv_028/issuecitymonitor/controller/PdfController.java
+++ src/main/java/ua/softserve/rv_028/issuecitymonitor/controller/PdfController.java
@@ -3,10 +3,20 @@ package ua.softserve.rv_028.issuecitymonitor.controller;
 import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+import java.io.FileOutputStream;
 
 @RestController
 @RequestMapping("/apis/pdf")
 public class PdfController {
+
+
+    public static void main(String...args){
+    }
+
+
     private static final Logger LOGGER = Logger.getLogger(PetitionController.class.getName());
 
     @GetMapping(path = "/issues")
@@ -33,7 +43,24 @@ public class PdfController {
     }
 
 
-    private void downloadPdf(String pdf){
-        LOGGER.info(pdf + " request successful!");
+    private void downloadPdf(String pdfName){
+
+
+        Document document = new Document();
+        try{
+            PdfWriter.getInstance(document, new FileOutputStream("src/main/resources/downloads/" + pdfName + ".pdf"));
+            document.open();
+            Paragraph p1 = new Paragraph("Hello World!  J");
+            Paragraph p2 = new Paragraph("Examsmyantra.com is committed to provide you valuable information and tutorials on various technologies.");
+            document.add(p1);
+            document.add(p2);
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+        document.close();
+
+
+        LOGGER.info(pdfName + " request successful! my TEST");
     }
 }
