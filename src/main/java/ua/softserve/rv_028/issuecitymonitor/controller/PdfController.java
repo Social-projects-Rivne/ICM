afceeb84c7b5cb7f14c9310719c43e415babe477
Evhenii Taranukha <tzhenia@MacBook-Air-tzhenia.local>
2018-01-18 10:38:40 +0200

    edit controller

--- src/main/java/ua/softserve/rv_028/issuecitymonitor/controller/PdfController.java
+++ src/main/java/ua/softserve/rv_028/issuecitymonitor/controller/PdfController.java
@@ -1,6 +1,7 @@
 package ua.softserve.rv_028.issuecitymonitor.controller;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
 import com.itextpdf.text.Document;
@@ -20,37 +21,39 @@ public class PdfController {
     private static final Logger LOGGER = Logger.getLogger(PetitionController.class.getName());
 
     @GetMapping(path = "/issues")
-    private void downloadPdfIssues() {
-        downloadPdf("issues");
+    private void createPdfIssues() {
+        createPdf("issues");
     }
 
 
     @GetMapping(path = "/petitions")
-    private void downloadPdfPetitions() {
-        downloadPdf("petitions");
+    private void createPdfPetitions() {
+        createPdf("petitions");
     }
 
 
     @GetMapping(path = "/events")
-    private void downloadPdfEvents() {
-        downloadPdf("events");
+    private void createPdfEvents() {
+        createPdf("events");
     }
 
 
     @GetMapping(path = "/users")
-    private void downloadPdfUsers() {
-        downloadPdf("users");
+    private void createPdfUsers() {
+        createPdf("users");
     }
 
 
-    private void downloadPdf(String pdfName){
+    @Value("${path.for.download}")
+    private String downloadPath;
+    private void createPdf(String pdfName){
 
 
         Document document = new Document();
         try{
-            PdfWriter.getInstance(document, new FileOutputStream("src/main/resources/downloads/" + pdfName + ".pdf"));
+            PdfWriter.getInstance(document, new FileOutputStream(downloadPath + pdfName + ".pdf"));
             document.open();
-            Paragraph p1 = new Paragraph("Hello World!  J");
+            Paragraph p1 = new Paragraph(pdfName);
             Paragraph p2 = new Paragraph("Examsmyantra.com is committed to provide you valuable information and tutorials on various technologies.");
             document.add(p1);
             document.add(p2);
--- src/main/java/ua/softserve/rv_028/issuecitymonitor/controller/PdfDownloadController.java
+++ src/main/java/ua/softserve/rv_028/issuecitymonitor/controller/PdfDownloadController.java
@@ -1,10 +1,13 @@
 package ua.softserve.rv_028.issuecitymonitor.controller;
 
+import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
 
 import javax.servlet.ServletContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
@@ -23,9 +26,12 @@ public class PdfDownloadController {
     @Autowired
     ServletContext context;
 
+    @Value("${path.for.download}")
+    private String downloadPath;
     @RequestMapping(value = "/pdf/{fileName:.+}", method = RequestMethod.GET, produces = "application/pdf")
     public ResponseEntity<InputStreamResource> download(@PathVariable("fileName") String fileName) throws IOException {
-        ClassPathResource pdfFile = new ClassPathResource("downloads/" + fileName);
+        FileReader pdfFile = new FileReader(downloadPath + fileName);
+        System.out.println(pdfFile.read());
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.parseMediaType("application/pdf"));
         headers.add("Access-Control-Allow-Origin", "*");
@@ -35,10 +41,9 @@ public class PdfDownloadController {
         headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
         headers.add("Pragma", "no-cache");
         headers.add("Expires", "0");
-
-        headers.setContentLength(pdfFile.contentLength());
+        InputStreamResource inputStreamResource = new InputStreamResource(new FileInputStream(downloadPath + fileName));
         ResponseEntity<InputStreamResource> response = new ResponseEntity<InputStreamResource>(
-                new InputStreamResource(pdfFile.getInputStream()), headers, HttpStatus.OK);
+                inputStreamResource, headers, HttpStatus.OK);
         return response;
     }
 
