965c9745b3054ecade0fb9012265d4cf6d6d6035
Vinni89 <Vinni89@ukr.net>
2018-02-01 16:40:50 +0200

    fix test create comment

--- src/test/java/ua/softserve/rv_028/issuecitymonitor/controller/CommentIssueControllerITest.java
+++ src/test/java/ua/softserve/rv_028/issuecitymonitor/controller/CommentIssueControllerITest.java
@@ -1,5 +1,6 @@
 package ua.softserve.rv_028.issuecitymonitor.controller;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,24 +9,46 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
 import ua.softserve.rv_028.issuecitymonitor.TestApplication;
+import ua.softserve.rv_028.issuecitymonitor.dao.IssueDao;
+import ua.softserve.rv_028.issuecitymonitor.dao.UserDao;
 import ua.softserve.rv_028.issuecitymonitor.dto.CommentIssueDto;
 import ua.softserve.rv_028.issuecitymonitor.entity.CommentIssue;
+import ua.softserve.rv_028.issuecitymonitor.entity.Issue;
+import ua.softserve.rv_028.issuecitymonitor.entity.User;
 import ua.softserve.rv_028.issuecitymonitor.service.mappers.CommentIssueMapper;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static ua.softserve.rv_028.issuecitymonitor.TestUtils.createIssue;
+import static ua.softserve.rv_028.issuecitymonitor.TestUtils.createUser;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = TestApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CommentIssueControllerITest {
 
     private CommentIssue commentIssue;
+    private Issue issue;
+    private User user;
+
+    @Autowired
+    private IssueDao issueDao;
+
+    @Autowired
+    private UserDao userDao;
 
     @Autowired
     private TestRestTemplate testRestTemplate;
 
     @Autowired
     private CommentIssueMapper commentIssueMapper;
+
+    @Before
+    public void setup(){
+        user = userDao.save(createUser(0));
+        issue = issueDao.save(createIssue(user, 0));
+        /*issues = issueDao.save(createIssuesList(user, LIST_SIZE));
+        issue = issues.get(0);*/
+    }
 
     @Test
     public void testListComment(){
