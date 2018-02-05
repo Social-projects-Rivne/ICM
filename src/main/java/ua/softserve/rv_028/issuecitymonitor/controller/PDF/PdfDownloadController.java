884b51cab50fc495159ec52d4241b832cbbd5d90
Evhenii Taranukha <tzhenia@MacBook-Air-tzhenia.local>
2018-02-05 15:42:33 +0200

    added GetMapping

--- src/main/java/ua/softserve/rv_028/issuecitymonitor/controller/PDF/PdfDownloadController.java
+++ src/main/java/ua/softserve/rv_028/issuecitymonitor/controller/PDF/PdfDownloadController.java
@@ -8,16 +8,12 @@ import javax.servlet.ServletContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -28,7 +24,7 @@ public class PdfDownloadController {
 
     @Value("${path.for.download}")
     private String downloadPath;
-    @RequestMapping(value = "/pdf/{fileName:.+}", method = RequestMethod.GET, produces = "application/pdf")
+    @GetMapping(value = "/pdf/{fileName:.+}", produces = "application/pdf")
     public ResponseEntity<InputStreamResource> download(@PathVariable("fileName") String fileName) throws IOException {
         FileReader pdfFile = new FileReader(downloadPath + fileName);
         HttpHeaders headers = new HttpHeaders();
