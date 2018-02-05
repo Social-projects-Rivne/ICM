4e2052e64f5c4978de1792e7f35fe96088636799
Evhenii Taranukha <tzhenia@MacBook-Air-tzhenia.local>
2018-02-05 16:32:43 +0200

    Added lombok annotation

--- src/main/java/ua/softserve/rv_028/issuecitymonitor/controller/PDF/DataObject.java
+++ src/main/java/ua/softserve/rv_028/issuecitymonitor/controller/PDF/DataObject.java
@@ -1,18 +1,26 @@
 package ua.softserve.rv_028.issuecitymonitor.controller.PDF;
 
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Setter
+@Getter
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class DataObject {
 
-    private String noteID;
-    private String title;
-    private String desc;
-    private String cat;
-    private String userID;
-    private String date;
-    private String regDate;
-    private String email;
-    private String phone;
-    private String role;
+    String noteID;
+    String title;
+    String desc;
+    String cat;
+    String userID;
+    String date;
+    String regDate;
+    String email;
+    String phone;
+    String role;
 
     public DataObject(PdfWritable writable) {
         this.setNoteID(Long.toString(writable.getId()));
@@ -35,75 +43,5 @@ public class DataObject {
         if (writable.getRole() != null) this.setRole(writable.getRole().toString());
         else this.setRole((null));
 
-    }
-
-    public String getNoteID() {
-        return noteID;
-    }
-    public void setNoteID(String noteID) {
-        this.noteID = noteID;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDesc() {
-        return desc;
-    }
-    public void setDesc(String desc) {
-        this.desc = desc;
-    }
-
-    public String getCat() {
-        return cat;
-    }
-    public void setCat(String cat) {
-        this.cat = cat;
-    }
-
-    public String getUserID() {
-        return userID;
-    }
-    public void setUserID(String userID) {
-        this.userID = userID;
-    }
-
-    public String getDate() {
-        return date;
-    }
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public String getRegDate() {
-        return regDate;
-    }
-    public void setRegDate(String regDate) {
-        this.regDate = regDate;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getRole() {
-        return role;
-    }
-    public void setRole(String role) {
-        this.role = role;
     }
 }
