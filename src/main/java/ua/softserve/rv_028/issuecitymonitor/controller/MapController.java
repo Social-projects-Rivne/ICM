2b347180814ba57860311c36bdcdd0757bd784b1
MSennikov <koliasen73@gmail.com>
2018-02-07 14:57:49 +0200

    Comit before merge

--- src/main/java/ua/softserve/rv_028/issuecitymonitor/controller/MapController.java
+++ src/main/java/ua/softserve/rv_028/issuecitymonitor/controller/MapController.java
@@ -1,24 +1,21 @@
 package ua.softserve.rv_028.issuecitymonitor.controller;
 
-import com.google.common.io.ByteStreams;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j;
-import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.CacheControl;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ua.softserve.rv_028.issuecitymonitor.dto.IssueDto;
-import ua.softserve.rv_028.issuecitymonitor.entity.Issue;
 import ua.softserve.rv_028.issuecitymonitor.service.IssueService;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
